f797dd779c0784a5e13b036665171d330959d4bb
sue <sue@MacBook-Pro-de-sue.local>
2020-08-11 15:31:44 -0500

    add formik dependence and managing from state

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,18 +1,34 @@
 import React from 'react';
+import { useFormik } from 'formik';
 
 const YoutubeForm = () => {
+// nos regresa un objeto que vamos a almacenar en una constante llamadda formik
+// Este contiene las propiedades para manejar el estado de nuesstro form
+ //1. pasa props inicial values
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      channel: '',
+    }
+  })
+  // 2. debemos cambiar el valor de la propiedaad incial de canda uno de los campos
+
+  console.log('form values:', formik.values);
   return (
     <div className="Youtube-main-container">
       <form className="YoutubeForm">
         <label htmlFor='name'>Name</label>
-        <input type='text' id='name' name='name' />
+        {/* usamos la propiedad handleChange que viene de los istanciado en formik */}
+        {/* formik traera automaticamente los valores por nosotros. */}
+        {/* el value del input debe corresponder la misma propuedad name del input */}
+        <input type='text' id='name' name='name' onChange={formik.handleChange} value={formik.values.name} />
 
         <label htmlFor='name'>Email</label>
-        <input type='email' id='email' name='email' />
+        <input type='email' id='email' name='email'onChange={formik.handleChange} value={formik.values.email} />
 
         <label htmlFor='name'>Channel</label>
-        <input type='text' id='channel' name='channel' />
-        
+        <input type='text' id='channel' name='channel'onChange={formik.handleChange} value={formik.values.channel}/>   
         <button className="submitButton">Submit</button>
       </form>
     </div>
