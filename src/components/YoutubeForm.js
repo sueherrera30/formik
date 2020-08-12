d4dbb4051682c070d46b9ce68192c648fa6d4701
sue <sue@MacBook-Pro-de-sue.local>
2020-08-12 15:33:51 -0500

    practicing encryptation

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -2,14 +2,10 @@ import React from 'react';
 import { useFormik } from 'formik';
 import { JSEncrypt } from 'jsencrypt';
 
-const pub_key='MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCV/2oXFKUOoXqxEGheZrrl+3KmU2zj0oP+rRDE+Cy5JEbMX1BGy3KLOZAgkpXyWlXgVCDEwMlsc1502ipGdWyq1cW4baV+wfxSVhR8jFT4dr+3lyJZgbDJTcFbVY8SwctKns9jtE8xRDWNXjJjY91xDFvpjiL6LkcuI1k5V9s8hQIDAQAB';
-
-const priv_key='MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAJX/ahcUpQ6herEQaF5muuX7cqZTbOPSg/6tEMT4LLkkRsxfUEbLcos5kCCSlfJaVeBUIMTAyWxzXnTaKkZ1bKrVxbhtpX7B/FJWFHyMVPh2v7eXIlmBsMlNwVtVjxLBy0qez2O0TzFENY1eMmNj3XEMW+mOIvouRy4jWTlX2zyFAgMBAAECgYBRMFOrNIh2197CEmB7oF1PeH/BGWzaD6tUYTavdqk2eTU9D+Yyu5I/W6PwjYdOC6lp5/9on/Uml3esBh5Vzt9G8ICFSlZLBEhVqKvW3HvTrkVVFl5yTF81dB3ysr1sV24t5mphG5NIReT/w6LbIdiZ5phj7YM19QbllI0sul2nYQJBAMmaPzzMHDZqKhNCJVmTcuDKVoybUerPyHje5rFj7KmE54fcw+eV45QOLC7yjL2cIcMQjQJtFoJ80niIlRT6qn0CQQC+eIzDgP3WoxhtaybOvb0yiH10pWDl5hPwb23YaObpxgySJJqL1c20ciIc3SL2KTUjefpcyxpsQVRCdmTjEnCpAkEAt8qiaifedJE2yJXd/RraeSqd2tPiy4IgTmlPwVrKsATVG1xdUSyp7Bk7mJVRT68ebHoTQ6n2AoIPUwmZeCXJ9QJAaqKtFCtccdU/sMqjoG1jxi3aJvsLw8pK8XpBAj7AwLKriQpLJ+ycTOD1Ljm0ACLT6De7LkZbVa5BkTAdjW9eeQJAbHFi8WOsZfqvCgMNmQ1jlQgErmpUYoAlc+q4lV7eUIUAvLHOdrWgwK6eqVgozvznOJfZK9iVjtr/tzRrFyi4gw==';
-
+const pub_key = process.env.REACT_APP_PUB_KEY;
+const priv_key = process.env.REACT_APP_PRIV_KEY;
+console.log(pub_key);
 const YoutubeForm = () => {
-
-  const testingTest = "Hola james bond";
-
   const formik = useFormik({
     initialValues: {
       name: '',
@@ -17,42 +13,19 @@ const YoutubeForm = () => {
       channel: '',
     },
     onSubmit: values => {
-      console.log('form data:', values);
+      console.log('the message before', values)
+      const encrypt = new JSEncrypt()
+      encrypt.setPublicKey(pub_key)
+      let encryptedLoginData = encrypt.encrypt(JSON.stringify(values))
+      console.log('send to server <3', encryptedLoginData)
+    
+      const decrypt = new JSEncrypt()
+      decrypt.setPrivateKey(priv_key)
+      let serverGetData = decrypt.decrypt(encryptedLoginData)
+      console.log('server GetData :o',JSON.parse(serverGetData))
     },
   })
 
-  // handleLoginClick = () => {
-  //   const encrypt = new JSEncrypt()
-  //   encrypt.setPublicKey(pub_key)
-
-  //   let params = {
-  //     account: this.account.value,
-  //     password: this.password.value,
-  //     date: new Date(),
-  //     random:Math.random(5),
-  //   }
-  //   console.log(testingTest)
-  //   let encryptedLoginData = encrypt.encrypt(JSON.stringify(testingTest))
-  //   console.log('sendToServer :D', encryptedLoginData)
-
-  //   // now on Server
-  //   const decrypt = new JSEncrypt()
-  //   decrypt.setPrivateKey(priv_key)
-  //   let serverGetData = decrypt.decrypt(encryptedLoginData)
-  //   console.log('serverGetData :o',JSON.parse(serverGetData))
-  // }
- 
-  const encrypt = new JSEncrypt()
-  encrypt.setPublicKey(pub_key)
-  console.log(testingTest)
-  let encryptedLoginData = encrypt.encrypt(JSON.stringify(testingTest))
-  console.log('sendToServer :D', encryptedLoginData)
-
-  // now on Server
-  const decrypt = new JSEncrypt()
-  decrypt.setPrivateKey(priv_key)
-  let serverGetData = decrypt.decrypt(encryptedLoginData)
-  console.log('serverGetData :o',JSON.parse(serverGetData))
 
   return (
     <div className="Youtube-main-container">
