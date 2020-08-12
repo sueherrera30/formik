fde5a0f5fbe37a7c2ccec49932e4d2b1310acd83
sue <sue@MacBook-Pro-de-sue.local>
2020-08-12 13:46:51 -0500

    modify youtube form

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -2,26 +2,14 @@ import React from 'react';
 import { useFormik } from 'formik';
 import { JSEncrypt } from 'jsencrypt';
 
+const pub_key='MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCV/2oXFKUOoXqxEGheZrrl+3KmU2zj0oP+rRDE+Cy5JEbMX1BGy3KLOZAgkpXyWlXgVCDEwMlsc1502ipGdWyq1cW4baV+wfxSVhR8jFT4dr+3lyJZgbDJTcFbVY8SwctKns9jtE8xRDWNXjJjY91xDFvpjiL6LkcuI1k5V9s8hQIDAQAB';
+
+const priv_key='MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAJX/ahcUpQ6herEQaF5muuX7cqZTbOPSg/6tEMT4LLkkRsxfUEbLcos5kCCSlfJaVeBUIMTAyWxzXnTaKkZ1bKrVxbhtpX7B/FJWFHyMVPh2v7eXIlmBsMlNwVtVjxLBy0qez2O0TzFENY1eMmNj3XEMW+mOIvouRy4jWTlX2zyFAgMBAAECgYBRMFOrNIh2197CEmB7oF1PeH/BGWzaD6tUYTavdqk2eTU9D+Yyu5I/W6PwjYdOC6lp5/9on/Uml3esBh5Vzt9G8ICFSlZLBEhVqKvW3HvTrkVVFl5yTF81dB3ysr1sV24t5mphG5NIReT/w6LbIdiZ5phj7YM19QbllI0sul2nYQJBAMmaPzzMHDZqKhNCJVmTcuDKVoybUerPyHje5rFj7KmE54fcw+eV45QOLC7yjL2cIcMQjQJtFoJ80niIlRT6qn0CQQC+eIzDgP3WoxhtaybOvb0yiH10pWDl5hPwb23YaObpxgySJJqL1c20ciIc3SL2KTUjefpcyxpsQVRCdmTjEnCpAkEAt8qiaifedJE2yJXd/RraeSqd2tPiy4IgTmlPwVrKsATVG1xdUSyp7Bk7mJVRT68ebHoTQ6n2AoIPUwmZeCXJ9QJAaqKtFCtccdU/sMqjoG1jxi3aJvsLw8pK8XpBAj7AwLKriQpLJ+ycTOD1Ljm0ACLT6De7LkZbVa5BkTAdjW9eeQJAbHFi8WOsZfqvCgMNmQ1jlQgErmpUYoAlc+q4lV7eUIUAvLHOdrWgwK6eqVgozvznOJfZK9iVjtr/tzRrFyi4gw==';
 
 const YoutubeForm = () => {
-  const message = 'Hello jaimito';
-  const uri = process.env.PUBLIC_KEY;
 
-//   const messageJson = JSON.stringify({
-//     Message: message,
-// });
-// const encrypt = new jsencrypt.JSEncrypt();
-const encrypt = new JSEncrypt();
-encrypt.setPublicKey(uri);
-// const  encryptedMessageJson = encrypt.encrypt(messageJson);
-const  encryptedMessageJson = encrypt.encrypt(message);
-const wrapperJson = JSON.stringify({ encryptedMessageJson });
+  const testingTest = "Hola james bond";
 
-console.log('encriptado', wrapperJson);
-
-// nos regresa un objeto que vamos a almacenar en una constante llamadda formik
-// Este contiene las propiedades para manejar el estado de nuesstro form
- //1. pasa props inicial values
   const formik = useFormik({
     initialValues: {
       name: '',
@@ -32,22 +20,44 @@ console.log('encriptado', wrapperJson);
       console.log('form data:', values);
     },
   })
-  // 2. debemos cambiar el valor de la propiedaad incial de canda uno de los campos
-  // value nos dará acceso al form data.
-  // console.log('form values:', formik.values);
 
-  // manejo de submit,son 2 pasos:
-  //esspecificar evento de submit, que ya nos ayudará a manejarlo formik
-  //2. teenemos que revisar el objeto que estamos pasando,agregamos a formik variable, propiedaad onsubmit
-  // la cual recibirá automaticamente el stado del formulario automaticamente, el cuall recibe a values como aargumento,
-  //usando una arrow fuction.
+  // handleLoginClick = () => {
+  //   const encrypt = new JSEncrypt()
+  //   encrypt.setPublicKey(pub_key)
+
+  //   let params = {
+  //     account: this.account.value,
+  //     password: this.password.value,
+  //     date: new Date(),
+  //     random:Math.random(5),
+  //   }
+  //   console.log(testingTest)
+  //   let encryptedLoginData = encrypt.encrypt(JSON.stringify(testingTest))
+  //   console.log('sendToServer :D', encryptedLoginData)
+
+  //   // now on Server
+  //   const decrypt = new JSEncrypt()
+  //   decrypt.setPrivateKey(priv_key)
+  //   let serverGetData = decrypt.decrypt(encryptedLoginData)
+  //   console.log('serverGetData :o',JSON.parse(serverGetData))
+  // }
+ 
+  const encrypt = new JSEncrypt()
+  encrypt.setPublicKey(pub_key)
+  console.log(testingTest)
+  let encryptedLoginData = encrypt.encrypt(JSON.stringify(testingTest))
+  console.log('sendToServer :D', encryptedLoginData)
+
+  // now on Server
+  const decrypt = new JSEncrypt()
+  decrypt.setPrivateKey(priv_key)
+  let serverGetData = decrypt.decrypt(encryptedLoginData)
+  console.log('serverGetData :o',JSON.parse(serverGetData))
+
   return (
     <div className="Youtube-main-container">
       <form className="YoutubeForm" onSubmit={formik.handleSubmit}>
         <label htmlFor='name'>Name</label>
-        {/* usamos la propiedad handleChange que viene de los istanciado en formik */}
-        {/* formik traera automaticamente los valores por nosotros. */}
-        {/* el value del input debe corresponder la misma propuedad name del input */}
         <input type='text' id='name' name='name' onChange={formik.handleChange} value={formik.values.name} />
 
         <label htmlFor='name'>Email</label>
