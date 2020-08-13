8f50117298658408b04f36ee395982dedd496a38
sue <sue@MacBook-Pro-de-sue.local>
2020-08-12 20:29:46 -0500

    first part of vaalidation

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -4,27 +4,48 @@ import { JSEncrypt } from 'jsencrypt';
 
 const pub_key = process.env.REACT_APP_PUB_KEY;
 const priv_key = process.env.REACT_APP_PRIV_KEY;
-console.log(pub_key);
+
+const initialValues =  {
+  name: '',
+  email: '',
+  channel: '',
+};
+
+const onSubmit = values => {
+  console.log('the message before', values);
+  const encrypt = new JSEncrypt();
+  encrypt.setPublicKey(pub_key);
+  let encryptedLoginData = encrypt.encrypt(JSON.stringify(values));
+  console.log('send to server <3', encryptedLoginData);
+
+  const decrypt = new JSEncrypt();
+  decrypt.setPrivateKey(priv_key);
+  let serverGetData = decrypt.decrypt(encryptedLoginData);
+  console.log('server GetData :o',JSON.parse(serverGetData));
+};
+
+const validate =  values => {
+  let errors = {}
+  if(!values.name) {
+    errors.name = 'Required';
+  }
+  if(!values.email) {
+    errors.email = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+       errors.email = 'Invalid email format';
+   }
+  if(!values.channel) {
+    errors.channel = 'Required';
+  }
+  return errors;
+};
+
 const YoutubeForm = () => {
   const formik = useFormik({
-    initialValues: {
-      name: '',
-      email: '',
-      channel: '',
-    },
-    onSubmit: values => {
-      console.log('the message before', values)
-      const encrypt = new JSEncrypt()
-      encrypt.setPublicKey(pub_key)
-      let encryptedLoginData = encrypt.encrypt(JSON.stringify(values))
-      console.log('send to server <3', encryptedLoginData)
-    
-      const decrypt = new JSEncrypt()
-      decrypt.setPrivateKey(priv_key)
-      let serverGetData = decrypt.decrypt(encryptedLoginData)
-      console.log('server GetData :o',JSON.parse(serverGetData))
-    },
-  })
+    initialValues,
+    onSubmit,
+    validate
+  });
 
 
   return (
